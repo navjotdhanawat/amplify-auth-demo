367ba93624b31ab6c8835e5aa1525069cac088bd
Navjot Dhanawat <navjotdhanawat@gmail.com>
2021-02-23 11:58:35 +0530

    Updated cred

--- src/index.js
+++ src/index.js
@@ -6,19 +6,25 @@ import * as serviceWorker from './serviceWorker';
 
 import Amplify from 'aws-amplify'
 const oauth = {
-    domain: 'auth8804.auth.ap-south-1.amazoncognito.com',
-    scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+    domain: 'navjottest1.auth.ap-south-1.amazoncognito.com',
+    scope: [
+        'phone',
+        'email',
+        'profile',
+        'openid',
+        'aws.cognito.signin.user.admin'
+    ],
     redirectSignIn: 'http://localhost:3000/',
     redirectSignOut: 'http://localhost:3000/',
-    responseType: 'code'
-  };
+    responseType: 'token'
+};
 Amplify.configure({
     Auth: {
         oauth: oauth,
-        identityPoolId: 'ap-south-1:4bf92106-96ea-4029-943c-a1993b10ba94',  // (Federated Identities > Selected Identity Pool/Create new > Sample code > Select Javascript > Get AWS Credentials)
+        identityPoolId: 'ap-south-1:7ab7150f-8703-4b9d-89eb-f1018e5dd2c4',  // (Federated Identities > Selected Identity Pool/Create new > Sample code > Select Javascript > Get AWS Credentials)
         region: 'ap-south-1', // (User pools > General Settings > Pool Id) The first part of the Pool Id us-east-1
-        userPoolId: 'ap-south-1_HnRx9QnN1', // (User pools > General Settings > Pool Id)
-        userPoolWebClientId: '25ivmj0thr0o32uhu2nq77dumo', // (User pools > General Settings > App clients > App client id)
+        userPoolId: 'ap-south-1_jlPdqBqBo', // (User pools > General Settings > Pool Id)
+        userPoolWebClientId: '5pep6hr8698icl4u0mhha55c8l', // (User pools > General Settings > App clients > App client id)
     },
 });
 
